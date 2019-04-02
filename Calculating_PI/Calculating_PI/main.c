1e71f25e76c83e72357d316d671cce29cbdf9c09
pfra17 <47630034+pfra17@users.noreply.github.com>
2019-04-02 20:52:11 +0200

    v0.2

--- Calculating_PI/Calculating_PI/main.c
+++ Calculating_PI/Calculating_PI/main.c
@@ -31,18 +31,16 @@
 
 extern void vApplicationIdleHook( void );
 void vInterface(void *pvParameters);
-double vCalculation(void *pvParameters);
+void vCalculation(void *pvParameters);
 
 double dPi4 = 1;
 uint64_t i=0;
-
-//void vLedBlink(void *pvParameters);
+char str[80];
 
 TaskHandle_t InterfaceTask;
 TaskHandle_t CalculationTask;
 TaskHandle_t ledTask;
 
-//calcEventGroup = xEventGroupCreate();
 EventGroupHandle_t calcEventGroup;
 void vApplicationIdleHook( void )
 {	
@@ -51,8 +49,7 @@ void vApplicationIdleHook( void )
 
 int main(void)
 {
-    resetReason_t reason = getResetReason();
-
+	calcEventGroup = xEventGroupCreate();
 	vInitClock();
 	vInitDisplay();
 	
@@ -62,53 +59,29 @@ int main(void)
 	
 
 	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"FreeRTOS 10.0.1");
-	vDisplayWriteStringAtPos(1,0,"EDUBoard 1.0");
-	vDisplayWriteStringAtPos(2,0,"Template");
-	vDisplayWriteStringAtPos(3,0,"ResetReason: %d", reason);
+	vDisplayWriteStringAtPos(0, 0, "Calculating Pi v0.1");
 	vTaskStartScheduler();
 	return 0;
 }
-
-/* void vLedBlink(void *pvParameters) {
-	(void) pvParameters;
-	PORTF.DIRSET = PIN0_bm; /*LED1
-	PORTF.OUT = 0x01;
-	for(;;) {
-// 		uint32_t stack = get_mem_unused();
-// 		uint32_t heap = xPortGetFreeHeapSize();
-// 		uint32_t taskStack = uxTaskGetStackHighWaterMark(ledTask);
-// 		vDisplayClear();
-// 		vDisplayWriteStringAtPos(0,0,"Stack: %d", stack);
-// 		vDisplayWriteStringAtPos(1,0,"Heap: %d", heap);
-// 		vDisplayWriteStringAtPos(2,0,"TaskStack: %d", taskStack);
-// 		vDisplayWriteStringAtPos(3,0,"FreeSpace: %d", stack+heap);
-		PORTF.OUTTGL = 0x01;				
-		vTaskDelay(100 / portTICK_RATE_MS);
-	}
-}
-*/
 	
 void vInterface(void *pvParameters) {
-	initButtons();
-	vTaskDelay(500);
-	vDisplayClear();
-	vDisplayWriteStringAtPos(0, 0, "Calculating Pi4 v1.0");
-	vDisplayWriteStringAtPos(1, 0, "Iterations: %d", i);
-	vDisplayWriteStringAtPos(2, 0, "Pi Value:");
-	for (;;) {
-		
+	for(;;) {	
+		initButtons();
+		vTaskDelay(500);
+		if (CALC_DONE_EVENT) {
+			vDisplayWriteStringAtPos(1, 0, "Iterations: %d", i);
+			vDisplayWriteStringAtPos(2, 0, "Pi Value:");
+		}
+		xEventGroupClearBits(calcEventGroup, CALC_DONE_EVENT);
 	}
-
-	
 }
 
-double vCalculation(void *pvParameters) {
+void vCalculation(void *pvParameters) {
 	for (;;) {	
 		for (i=0; i<=1e6; i++) {
 			dPi4 = dPi4 - 1.0/(3+i*4) + 1.0/(5+i*4);
 			dPi4 *= 4; //To make Pi out of Pi/4
-			
+			xEventGroupSetBits(calcEventGroup, CALC_DONE_EVENT);
 		}
 	}
 }
